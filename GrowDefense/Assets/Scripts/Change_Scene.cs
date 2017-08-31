db5d479da141a2e853ab3d065b81db2d61c133f4
LAPTOP-H5PPJI5D\njack <njacksonco@gmail.com>
2017-08-31 16:30:19 -0600

    Created more game assets, and started player movement.

---
+++ GrowDefense/Assets/Scripts/Change_Scene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Change_Scene : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Load_Scene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit_Game()
+    {
+        
+    }
+}
