aa002404340fe57e6d1ed7ae543b9c2ef13859a4
NickJackson525 <njacksonco>
2017-12-05 16:38:36 -0700

    started working on help popups

---
+++ GrowDefense/Assets/Scripts/UI/HelpPopup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPopup : MonoBehaviour
+{
+    public enum HelpType { DRYPLANTS}
+    public HelpType thisHelpType = HelpType.DRYPLANTS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
